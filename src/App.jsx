2941e06edeeb7aa35ac1a96e64904da7a3d1ba7c
Juan Ferreyra <juan.ferreyra1@outlook.com>
2022-05-31 23:16:34 -0300

    Primer NavBar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import Cuerpo from './components/Cuerpo/Cuerpo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+
+    <NavBar/>
+    <Cuerpo/>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap'
+
+function NavBar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">JT Indumentaria</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Inicio</Nav.Link>
+                    <Nav.Link href="#link">Preguntas Frecuentes</Nav.Link>
+                    <NavDropdown title="Prendas" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Abrigos</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Remeras</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Pantalones</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Tabla de talles</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+  }
+
+  export default NavBar;
